73973d555ca7024d61e0326ad9977e8d1e7b0d7d
Veenu Yadav <c=veenu_yadav@mckinsey.com>
2022-02-03 21:11:35 +0530

    7. github Actions

--- src/setup.py
+++ src/setup.py
@@ -1,8 +1,6 @@
 from setuptools import find_packages, setup
 
-entry_point = (
-    "kedro_proj_learning = project_ops_next_analytics.__main__:main"
-)
+entry_point = "kedro_proj_learning = project_ops_next_analytics.__main__:main"
 
 
 # get the dependencies and installs
